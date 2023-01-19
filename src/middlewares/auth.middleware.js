40aaafd4450f77531348b7c27ad0365defa44531
JamiroB <jbances7@gmail.com>
2023-01-18 20:22:35 -0500

    updated

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,27 +4,32 @@ require("dotenv").config();
 const authMiddleware = (req, res, next) => {
   let { authorization: token } = req.headers; //es lo mismo del codigo de abajo
   /* const token = req.headers.authorization; */
-  token = token.replace("Bearer ", "");
+  token = token?.replace("Bearer ", "");
   console.log(token);
-  const decoded = jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    { algorithms: "HS512" },
-    (err, decoded) => {
-      if (err) {
-        res.status(400).json({
-          error: "Invalid token",
-          message:
-            "The token is not valid or is already expired, send a right token.",
-        });
-      } else {
-        console.log(decoded);
-        next();
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { algorithms: "HS512" },
+      (err, decoded) => {
+        if (err) {
+          res.status(400).json({
+            error: "Invalid token",
+            message:
+              "The token is not valid or is already expired, send a right token.",
+          });
+        } else {
+          console.log(decoded);
+          next();
+        }
       }
-    }
-  );
-  console.log(decoded);
-  /* next(); */
+    );
+  }else{
+    res.status(400).json({
+      error: "No token provided",
+      message: "No se esta enviando un token de autenticación"
+    });
+  }
 };
 
 module.exports = authMiddleware;
